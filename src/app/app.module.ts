b8f2c251a673471610ed90d5edd466f000ee7c6b
JM_GitLab_WGU <jmo2395@wgu.edu>
2023-06-17 15:21:48 -0400

    Add base components for map and country info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MapComponentComponent } from './map-component/map-component.component';
+import { CountryInfoComponentComponent } from './country-info-component/country-info-component.component';
+import { MapComponent } from './map/map.component';
+import { CountryInfoComponent } from './country-info/country-info.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponentComponent,
+    CountryInfoComponentComponent,
+    MapComponent,
+    CountryInfoComponent
   ],
   imports: [
     BrowserModule,
