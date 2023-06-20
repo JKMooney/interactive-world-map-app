8bde91c15f3b67f200b187e275bce53cde089c68
JM_GitLab_WGU <jmo2395@wgu.edu>
2023-06-20 06:57:33 -0400

    update: default route added to map component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // set default route to the map component
+  {path: '', component: MapComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
