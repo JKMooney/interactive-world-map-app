a24ae9d11de076688c130710d2f0d095b3775037
JM_GitLab_WGU <jmo2395@wgu.edu>
2023-06-20 07:05:50 -0400

    update: app component styled and logic added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { WorldbankService } from './worldbank.service';
+import { CountryInfo } from './models/country-info';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private worldbankService: WorldbankService) { }
+
   title = 'interactive_world_map_app';
+
+  selectedCountryInfo?: CountryInfo;
+  onCountryClick(countryCode: string) {
+    this.worldbankService.getData(countryCode).subscribe(response => {
+      if (response == null) {
+        console.warn('No country found for code: ' + countryCode);
+      } else {
+        console.log(response);
+        const countriesList = response[1];
+        const firstCountry = countriesList[0];
+        this.selectedCountryInfo = firstCountry;
+      }
+    });
+  }
 }
