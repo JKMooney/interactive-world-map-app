5fa759ebf60bc0fdfa0dc6fa21bdca12e98b4069
JM_GitLab_WGU <jmo2395@wgu.edu>
2023-06-20 06:56:20 -0400

    add HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MapComponentComponent } from './map-component/map-component.component';
-import { CountryInfoComponentComponent } from './country-info-component/country-info-component.component';
 import { MapComponent } from './map/map.component';
 import { CountryInfoComponent } from './country-info/country-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MapComponentComponent,
-    CountryInfoComponentComponent,
     MapComponent,
     CountryInfoComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
